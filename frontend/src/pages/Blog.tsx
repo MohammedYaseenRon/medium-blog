4c180a6a54d71a5792ac611487ad4bd2c4a64e8a
MohammedYaseenRon <yaseenron3@gmail.com>
2024-08-17 11:10:24 +0530

    changes made

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -11,7 +11,7 @@ export const Blog = () =>{
     const {loading, blog} = useBlog({
         id: id || ""
     });
-    if(loading) {
+    if(loading || !blog) {
         return <div>
             <Appbar />
             <div className="h-screen flex flex-col justify-center">
